7843d08401ef97f8cdfe7e49f045bf5101eae11a
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-09-22 00:46:09 -0400

    Implement run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=jllovet/udacitydevops
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+# Create deployment with external service for load balancer
+kubectl apply -f jllovet-udacitydevops.yaml
 
 # Step 3:
 # List kubernetes pods
+kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-
+# Resource found here for port forwarding from localhost
+# https://alesnosek.com/blog/2017/02/14/accessing-kubernetes-pods-from-outside-of-the-cluster/
+kubectl port-forward service/jllovet-udacitydevops-service 8000:8000
