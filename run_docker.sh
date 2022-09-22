41f1241ed5a07d329f56c43620e10baac7ace8c1
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-09-21 23:26:51 -0400

    Implement run_docker

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker image build . --tag jllovet/udacitydevops:latest
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker container run -d -p 8000:80 --name=predictor jllovet/udacitydevops
